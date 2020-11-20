ae3c4579febbdb102176d99119be09b40cf5fce6
fatihcandev <fatihcan.social@gmail.com>
2020-11-20 14:27:55 +0300

    add navigation types

--- src/navigation/AuthNavigator.tsx
+++ src/navigation/AuthNavigator.tsx
@@ -3,14 +3,27 @@ import { createStackNavigator } from "@react-navigation/stack";
 
 import Onboarding from "../views/auth/Onboarding";
 import Welcome from "../views/auth/Welcome";
+import { AuthRoutes } from "../types";
 
-const AuthStack = createStackNavigator();
+const AuthStack = createStackNavigator<AuthRoutes>();
 
 const AuthNavigator: React.FC = () => {
   return (
-    <AuthStack.Navigator headerMode="none">
-      <AuthStack.Screen name="Onboarding" component={Onboarding} />
-      <AuthStack.Screen name="Welcome" component={Welcome} />
+    <AuthStack.Navigator headerMode="screen">
+      <AuthStack.Screen
+        name="Onboarding"
+        component={Onboarding}
+        options={{ headerShown: false }}
+      />
+      <AuthStack.Screen
+        name="Welcome"
+        component={Welcome}
+        options={{
+          headerShown: true,
+          headerTitle: "",
+          headerTransparent: true,
+        }}
+      />
     </AuthStack.Navigator>
   );
 };

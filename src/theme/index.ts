6821e0f20f9fedf33336c4b52cd5a4523564dda6
fatihcandev <fatihcan.social@gmail.com>
2020-11-20 13:11:41 +0300

    add border radius to theme

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -14,6 +14,12 @@ const theme = createTheme({
     l: 24,
     xl: 40,
   },
+  borderRadii: {
+    s: 4,
+    m: 10,
+    l: 25,
+    xl: 75,
+  },
   textVariants: {
     hero: {
       fontSize: 80,
